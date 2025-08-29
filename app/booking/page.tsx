b802a4fe118c424a8d2d4c8693fbb2b684429ce2
Karen <leonardomoreiraemp@gmail.com>
2025-08-29 15:22:58 +0400

    jfk

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Template from './Template'
+// import Template from './Template'
 
 const page = () => {
-  
-  return <Template />
+
+  return (<div>jfkdf</div>)
+  // return <Template />
   
 }
 
